eac00e2eaa2225c6b2c38c417656a8a4ae0aa3de
leticia-ssantos <leticia.intecnik@gmail.com>
2024-12-11 14:08:25 -0300

    feat: implementado GrpcConnection e IGrpcConnection

---
+++ Shared/Connection/GrpcConnection.cs
@@ -0,0 +1,20 @@
+﻿using Grpc.Net.Client;
+using System.Net.Http;
+using Grpc.Net.Client.Web;
+
+namespace Shared.Connection
+{
+    internal class GrpcConnection : IGrpcConnection
+    {
+        public GrpcChannel Channel { get; private set; }
+
+        public GrpcConnection(string address)
+        {
+            var options = new GrpcChannelOptions
+            {
+                HttpHandler = new GrpcWebHandler(new HttpClientHandler())
+            };
+            Channel = GrpcChannel.ForAddress(address, options);
+        }
+    }
+}
---
+++ Shared/Connection/IGrpcConnection.cs
@@ -0,0 +1,9 @@
+﻿using Grpc.Net.Client;
+
+namespace Shared.Connection
+{
+    public interface IGrpcConnection
+    {
+        GrpcChannel Channel { get; }
+    }
+}
