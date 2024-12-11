dc6c9bb6f64ab2f4da88d3cf59170657d8e17f55
leticia-ssantos <leticia.intecnik@gmail.com>
2024-12-11 15:28:03 -0300

    feat: adicionado "app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });" no Program do server

--- Server/Program.cs
+++ Server/Program.cs
@@ -8,13 +8,16 @@ builder.Services.AddGrpcReflection();
 
 var app = builder.Build();
 
+app.UseGrpcWeb(new GrpcWebOptions { DefaultEnabled = true });
+
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>().EnableGrpcWeb();
-app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 if (app.Environment.IsDevelopment())
 {
     app.MapGrpcReflectionService();
 }
+
+app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 app.Run();
